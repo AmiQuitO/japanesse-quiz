46b25084b7410234a2a5a503c3bd19743a0ca6f6
KonradHoffmannSilva <amiquitooriginal@gmail.com>
2022-02-20 22:38:32 +0100

    20-02-2022

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-x = 0;
+answer = 0;
 document.getElementById("menu").style.top = "0px";
 document.getElementById("game").style.display = "none";
 
@@ -17,37 +17,39 @@ function openmenu(){
 }
 function newgame(){
     document.getElementById("g_choices").innerHTML = "";
-    x = Math.floor(Math.random()*108); // the number you have to guess
-    x2 = Math.floor(Math.random()*4); // which button is the right one
-    x1 = []; // the choices
-    console.log(x, x2);
+    answer = Math.floor(Math.random()*108); // the number you have to guess
+    x = Math.floor(Math.random()*4); // which button is the right one
+    choices = []; // the choices
+    console.log(answer, x);
     for(i=0;i<4;i++){
         a = Math.floor(Math.random()*108);
         if(!(i == 0)){
             do{
                 cont = false;
                 for(j=0;j<4;j++){
-                    if(x1[j] == a || x1[j] == x){  
+                    if(choices[j] == a || choices[j] == answer){  
                         a = Math.floor(Math.random()*108);
                         cont = true;
                     }
                 }
             }while(cont);
         }
-        x1[i] = a;
+        choices[i] = a;
     }
-    x1[x2] = x; // overwrite the right answer
-    console.log(x1,x, x2);
-    document.getElementById("g_image").innerHTML = hira1[x] + (x2+1);
+    choices[x] = answer; // overwrite the right answer
+    console.log(choices,answer, x);
+    document.getElementById("g_image").innerHTML = hira1[answer] + (x+1);
     for(i=0;i<4;i++){
-        document.getElementById("g_choices").innerHTML += "<div class='g_buttons' onclick='answer("+ x1[i] +")'>"+ hira2[x1[i]] +"</div>";
+        document.getElementById("g_choices").innerHTML += "<div class='g_buttons' onclick='choose("+ choices[i] +")'>"+ hira2[choices[i]] +"</div>";
     }
 }
-function answer(a){
-    if(a == x){
+function choose(a){
+    if(a == answer){
         console.log("Won");
         newgame();
     }else{
         console.log("Lost");
     }
 }
+
+// gotta upgrade this code so its readible, but it works for now
